24e210096ade71d43c35675aa39088583d2070f5
Stephen Hebson <shebson@gmail.com>
2024-09-21 17:26:22 -0400

    Fix HttpsProxyAgent instantiation

--- index.js
+++ index.js
@@ -5,12 +5,9 @@ import { HttpsProxyAgent } from 'https-proxy-agent';
 const SERVER_PORT = process.env.PORT || 3000;
 
 async function getCurrentTimeViaFixie() {
+  const agent = new HttpsProxyAgent(process.env.FIXIE_URL);
   try {
-    const response = await fetch(`http://worldclockapi.com/api/json/utc/now`, {
-      agent: new HttpsProxyAgent({
-        proxy: process.env.FIXIE_URL
-      }),
-    });
+    const response = await fetch(`http://worldclockapi.com/api/json/utc/now`, { agent });
     const data = await response.json();
     return `The current UTC time is ${data.currentDateTime}. Request to worldclockapi.com made through Fixie.`;
   } catch (error) {
